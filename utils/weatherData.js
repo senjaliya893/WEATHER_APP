eb61850dee9bb850a32b6419925f3d4fd0eba904
ViVEK SENJALiYA <154584056+senjaliya893@users.noreply.github.com>
2024-07-23 17:26:52 +0530

    completed

---
+++ utils/weatherData.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+const openWeatherMap = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+  SECRET_KEY: "308c399b844e8150a4ec7b0a830605ea",
+};
+
+const weatherData = (address, callback) => {
+  const url =
+    openWeatherMap.BASE_URL +
+    encodeURIComponent(address) +
+    "&APPID=" +
+    openWeatherMap.SECRET_KEY;
+  console.log(url);
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback(true, "Unable to fetch data, Please try again. " + error);
+    }
+    callback(false, data?.body);
+  });
+};
+
+module.exports = weatherData;
